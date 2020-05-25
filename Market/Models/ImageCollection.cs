7fae6c452cb71b2b92db98c2aab6b8dfa46a063b
Doxira <Doxira@abv.bg>
2020-05-26 01:40:20 +0300

    More Bugs Coming

---
+++ Market/Models/ImageCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Market.Models
+{
+    public partial class ImageCollection
+    {
+        public int Id { get; set; }
+        public byte[] Picture1 { get; set; }
+        public byte[] Picture2 { get; set; }
+        public byte[] Picture3 { get; set; }
+        public byte[] Picture4 { get; set; }
+        public byte[] Picture5 { get; set; }
+    }
+}
---
+++ Market/Services/ICreatePostService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Market.Services
+{
+    public interface ICreatePostService
+    {
+        void CreatePost(string title, string description, decimal price, int quantity,string userId);
+    }
+}
